08766ed89f596c09da31382139d99b422a7af379
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2024-06-09 18:54:36 +0100

    Portfolio alternating project hover effects

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -23,8 +23,8 @@ export default function Portfolio() {
 
 function Project({ title, description, tags, imageUrl }: PortfolioProps) {
   return (
-    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative mb-3 sm:mb-8 last:mb-0 even:pl-8">
-      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] group-even:ml-[18rem]">
+    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden relative sm:h-[25rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
+      <div className="pt-4 pb-7 px-5 sm:pl-5 sm:pr-5 sm:pt-10 sm:max-w-[50%] group-even:ml-[20rem]">
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
         <ul className="flex flex-wrap gap-2 mt-4">
@@ -42,7 +42,7 @@ function Project({ title, description, tags, imageUrl }: PortfolioProps) {
         src={imageUrl}
         alt={title}
         quality={90}
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40"
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:rotate-2 group-hover:scale-105 transition"
       />
     </section>
   );
