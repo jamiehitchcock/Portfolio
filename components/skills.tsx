e3c54906a7ba63a735b4958838313255982ff8fa
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2024-06-17 20:09:56 +0100

    Skills section animate navigation with useSectionInView

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,11 +1,16 @@
+"use client"
+
 import React from "react";
 import SectionHeading from "./section-heading";
 import { skillsData } from "@/lib/data";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Skills() {
+        // custom hook for inView animation
+        const { ref } = useSectionInView("Skills");
 
   return (
-    <section className="mb-28 max-w-[50rem] sm:mb-40 scroll-mt-28">
+    <section ref={ref} id="skills" className="mb-28 max-w-[50rem] sm:mb-40 scroll-mt-28">
       <SectionHeading>Skills</SectionHeading>
       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
         {skillsData.map((skill, index) => (
