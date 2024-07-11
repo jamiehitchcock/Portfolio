4ff901820ce66e29e28c0b254669f36d668f48f0
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2024-07-11 22:14:00 +0100

    Hamburger Menu React Icons
    GR React Icons for Hamburger Menu on navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import { navLinks } from "@/lib/data";
 import Link from "next/link";
+import { GrMenu, GrClose } from "react-icons/gr";
 
 export default function Navbar() {
   const [isClick, setIsClick] = useState(false);
@@ -48,35 +49,9 @@ export default function Navbar() {
               >
                 {/* change icon on toggle */}
                 {isClick ? (
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
+                  <GrClose className="text-3xl"/>
                 ) : (
-                  <svg
-                    className="h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M4 6h16M4 12h16m-7 6h7"
-                    />
-                  </svg>
+                  <GrMenu className="text-3xl"/>
                 )}
               </button>
             </div>
