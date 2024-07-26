5a79dc7a002d656023754fce304ed2c49f022250
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2024-07-26 14:48:54 +0100

    Toggler for JSON BIO
    Set to hide and default, toggle button to show JSON bio if users are interested

--- components/about.tsx
+++ components/about.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
 import { useSectionInView } from "@/lib/hooks";
@@ -11,14 +11,20 @@ const jsonBio = {
     lastname: "Hitchcock",
   },
   title: "Front End Developer",
-  experience: "2 years",
-  favourite_languages: ["JavaScript", "React.js"],
-  location: "Near London",
-  can_not_live_without: "Coffee",
+  years_of_experience: 2,
+  location: "Reading",
+  work_preference: "Hybrid",
+  operating_systems: "Windows or MacOS",
+  favourite_language: "JavaScript",
+  currently_learning:"Next.js",
+  can_not_live_without: ["coffee", "my wife"],
   dream_car: "Porsche 911",
+  hobbies: ["record collecting", "travelling"],
 };
 
 export default function About() {
+  const [showJSON, setShowJSON] = useState(false);
+
   // custom hook for inView animation
   const { ref } = useSectionInView("About");
 
@@ -32,23 +38,39 @@ export default function About() {
       id="about"
     >
       <SectionHeading>About Me</SectionHeading>
-      <div className="flex flex-col items-center">
-        <pre className="mb-3 bg-stone-800 text-lime-500 w-fit">
-          <code>{JSON.stringify(jsonBio, undefined, 2)}</code>
-        </pre>
-      </div>
       <p className="mb-3">
         Thank you for visiting my portfolio. I love to write code and create
-        interactive websites. I graduated from a <b className="text-violet-700">Web Development Bootcamp</b> with
-        <b className="text-violet-700"> Bath Spa University / Institude Of Code</b> where I was awarded the
-        prize of <b className="text-violet-700">Star Pupil</b> and <b className="text-violet-700">Best Portfolio</b>. I regularly return
-        as a testimonial student to inspire new cohorts. Most of my knowledge
-        and skills are self taught and I am gripped by the challenges that
-        writing code brings. There is a special feeling that coding gives, when
-        you are immersed in what you are working on, as you watch it come to
-        life. When I am not working I love nothing more than to travel the
-        world.
+        interactive websites. I graduated from a&nbsp;
+        <b className="text-violet-700">Web Development Bootcamp</b> with
+        <b className="text-violet-700">
+          &nbsp; Bath Spa University / Institude Of Code&nbsp;
+        </b>
+        where I was awarded the prize of&nbsp;
+        <b className="text-violet-700">Star Pupil</b> and&nbsp;
+        <b className="text-violet-700">Best Portfolio</b>. I regularly return as
+        a testimonial student to inspire new cohorts. Most of my knowledge and
+        skills are self taught and I am gripped by the challenges that writing
+        code brings. There is a special feeling that coding gives, when you are
+        immersed in what you are working on, as you watch it come to life.
       </p>
+      <p className="text-center mb-3">
+        Click
+        <button
+          className="text-violet-700"
+          onClick={() => setShowJSON(!showJSON)}
+        >
+          &nbsp;here&nbsp;
+        </button>
+        to see a snapshot of me as a JSON object !
+      </p>
+
+      {showJSON && (
+        <div className="flex flex-col items-center">
+          <pre className="mb-3 bg-stone-800 text-lime-500 w-fit">
+            <code>{JSON.stringify(jsonBio, undefined, 2)}</code>
+          </pre>
+        </div>
+      )}
     </motion.section>
   );
 }
