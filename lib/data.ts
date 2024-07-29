21e914b79a02d89c5c0c7a5dcd9080477a761a72
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2024-07-29 17:26:23 +0100

    Portfolio Images - remove placeholder

--- lib/data.ts
+++ lib/data.ts
@@ -15,8 +15,14 @@ import {
   SiBootstrap,
 } from "react-icons/si";
 import { LuGraduationCap } from "react-icons/lu";
-
-import placeholderImage from "@/public/placeholderImage.png";
+import CommandCentre from "@/public/images/CommandCentre.jpeg";
+import FilmDB from "@/public/images/FilmDB.jpeg";
+import InternationalSpaceStationTracker from "@/public/images/InternationalSpaceStationTracker.jpeg";
+import PomodoroTimer from "@/public/images/PomodoroTimer.jpeg";
+import Calculator from "@/public/images/Calculator.jpeg";
+import RomanNumeralConvertor from "@/public/images/RomanNumeralConvertor.jpeg";
+import StockMarketChecker from "@/public/images/StockMarketChecker.jpeg";
+import JamieHitchcockPortfolio from "@/public/images/JamieHitchcockPortfolio.jpeg";
 
 export const navLinks = [
   {
@@ -67,29 +73,29 @@ export const experiencesData = [
 
 export const portfolioData = [
   {
-    title: "FilmDB",
-    description:
-      "A film app created using React to show a snapshot of a film, such as the poster, plot summary and a rating. Try searching for your favourite film.",
-    tags: ["React.js", "JavaScript", "SASS", "API"],
-    imageUrl: placeholderImage,
-    demo: "https://react-filmdb.netlify.app/",
-    code: "https://github.com/jamiehitchcock/FE-Lib-Training/tree/main/03-Practice-Project-2/React-Films-App",
-  },
-  {
     title: "Command Centre",
     description:
       "A companion app for a live service video game called Helldivers 2, created in React. This uses API calls to update users with real time data relating to Helldivers 2. Meaning that you can get the latest news whilst on the go.",
     tags: ["React.js", "JavaScript", "SASS", "API", "React Router"],
-    imageUrl: placeholderImage,
+    imageUrl: CommandCentre,
     demo: "https://command-centre.netlify.app",
     code: "https://github.com/jamiehitchcock/FE-Lib-Training/tree/main/02-API-Practice-Project/05-Sass-Command-Centre",
+  },
+  {
+    title: "FilmDB",
+    description:
+      "A film app created using React to show a snapshot of a film, such as the poster, plot summary and a rating. Try searching for your favourite film.",
+    tags: ["React.js", "JavaScript", "SASS", "API"],
+    imageUrl: FilmDB,
+    demo: "https://react-filmdb.netlify.app/",
+    code: "https://github.com/jamiehitchcock/FE-Lib-Training/tree/main/03-Practice-Project-2/React-Films-App",
   },
   {
     title: "International Space Station Tracker",
     description:
       "Track the location of the ISS against a world map in real time. This uses an API (WhereTheISS) to retrieve coordinates and velocity, then plots the location against a map using Leaflet.js and JavaScript.",
     tags: ["Leaflet.js", "JavaScript", "HTML", "CSS", "API"],
-    imageUrl: placeholderImage,
+    imageUrl: InternationalSpaceStationTracker,
     demo: "https://jamiehitchcock.co.uk/",
     code: "https://github.com/jamiehitchcock/Portfolio",
   },
@@ -98,7 +104,7 @@ export const portfolioData = [
     description:
       "A timer created for the pomodoro study method of 25 plus 5. I Created this in React as part of a 'Front-End Development Libraries' accreditation with FreeCodeCamp.",
     tags: ["React.js", "JavaScript", "SASS", "Complex State Management"],
-    imageUrl: placeholderImage,
+    imageUrl: PomodoroTimer,
     demo: "https://react-pomodoro-timer-jh.netlify.app",
     code: "https://github.com/jamiehitchcock/FCC_FE-Lib_Projects/tree/main/Pomodoro_Timer",
   },
@@ -113,7 +119,7 @@ export const portfolioData = [
       "Algorithms",
       "Complex State Management",
     ],
-    imageUrl: placeholderImage,
+    imageUrl: Calculator,
     demo: "https://react-calculator-jh.netlify.app/",
     code: "https://github.com/jamiehitchcock/FCC_FE-Lib_Projects/tree/main/Calculator",
   },
@@ -122,7 +128,7 @@ export const portfolioData = [
     description:
       "Enter a number and the algorithm will convert this into a Roman Numeral using Javascript. I created this as a challenge for part of a FreeCodeCamp accreditation on 'JavaScript Data Structures and Algorithyms.'",
     tags: ["JavaScript", "HTML", "CSS", "Algorithms"],
-    imageUrl: placeholderImage,
+    imageUrl: RomanNumeralConvertor,
     demo: "https://jamiehitchcock.co.uk/",
     code: "https://github.com/jamiehitchcock/Portfolio",
   },
@@ -131,7 +137,7 @@ export const portfolioData = [
     description:
       "This allows you to enter a symbol for a stock and view realtime information on its performance using an API (StockData). Try any public stock such as  AAPL, GOOG, PG or KO.",
     tags: ["JavaScript", "HTML", "CSS", "API"],
-    imageUrl: placeholderImage,
+    imageUrl: StockMarketChecker,
     demo: "https://jamiehitchcock.co.uk/",
     code: "https://github.com/jamiehitchcock/Portfolio",
   },
@@ -140,7 +146,7 @@ export const portfolioData = [
     description:
       "An improvement upon my first portfolio website, to test my skills in Next.js, React and Typescript. Styled using Tailwind.",
     tags: ["Next.js", "React.js", "TypeScript", "Tailwind", "Framer Motion"],
-    imageUrl: placeholderImage,
+    imageUrl: JamieHitchcockPortfolio,
     demo: "https://jamiehitchcock.co.uk/",
     code: "https://github.com/jamiehitchcock/Portfolio",
   },
