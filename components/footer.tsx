3e1674ed7ea708eceadc216785c3f29dc38d02a7
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2024-08-05 17:47:30 +0100

    footer margin bottom

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="text-center">
-      <h3 className="text-gray-950 text-2xl font-bold mb-3">
+      <h3 className="text-gray-950 text-2xl font-bold mb-8">
         <span className="text-violet-700">J</span>amie&nbsp;
         <span className="text-violet-700">H</span>itchcock
       </h3>
