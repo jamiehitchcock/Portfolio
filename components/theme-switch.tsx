46dca55af74d2a48d05bd5c4106b2ba70511597f
Jamie Hitchcock <jamiehitchcock@hotmail.co.uk>
2024-08-06 19:32:22 +0100

    useState to toggle theme and update local storage

--- components/theme-switch.tsx
+++ components/theme-switch.tsx
@@ -1,11 +1,41 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { BsSunFill, BsMoonFill } from "react-icons/bs";
 
+// typing as either light or dark theme
+type Theme = "light" | "dark";
+
 export default function ThemeSwitch() {
+  const [theme, setTheme] = useState<Theme>("light");
+
+//   change theme and update local storage
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme","dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme","light");
+    }
+  };
+
+  useEffect(() => {
+    //   read the local storage on load component
+      const localTheme = window.localStorage.getItem("theme") as Theme | null;
+
+    //   if local storage has been set, then update theme
+      if (localTheme){
+          setTheme(localTheme);
+      }
+  },[]);
+
   return (
-    <button className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all">
-      <BsSunFill />
-      {/* <BsMoonFill /> */}
+    <button
+      className="fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem] bg-opacity-80 backdrop-blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all"
+      onClick={toggleTheme}
+    >
+      {theme === "light" ? <BsSunFill /> : <BsMoonFill />}
     </button>
   );
 }
